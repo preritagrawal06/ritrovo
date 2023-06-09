c4096abf55fcbbbb5825a21ce7b23755f999d191
preritagrawal06 <agrawalprerit780@gmail.com>
2023-06-09 13:42:42 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() => runApp(const MaterialApp(
+  home: SignIn(),
+));
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: const Padding(
+          padding: EdgeInsets.fromLTRB(10.0, 5.0, 0.0, 0.0),
+          child: CircleAvatar(
+            radius: 15.0,
+            backgroundImage: AssetImage('assets/images/Ellipse 3.png'),
+          ),
+        ),
+        title: const Text(
+            'Login',
+          style: TextStyle(
+            fontSize: 23.0,
+            color: Colors.black,
+          ),
+        ),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 100.0,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30.0),
+                      child: Text(
+                          'Sign In',
+                        style: TextStyle(
+                          fontSize: 37.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                      child: Text(
+                        'Username/Email',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: 'Username',
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 10.0),
+                      child: Text(
+                        'Password',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        labelText: 'Password',
+                      ),
+                    ),
+                    TextButton(
+                        onPressed: (){},
+                        child: const Text(
+                            'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15.0,
+                            letterSpacing: 0.2,
+                          ),
+                        )
+                    ),
+                    FloatingActionButton(
+                        onPressed: (){},
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Icon(Icons.arrow_forward, color: Colors.white,),
+                      ),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 30.0),
+                        child: Row(
+                          children: [
+                            const Text(
+                                'New User?',
+                              style: TextStyle(
+                                fontSize: 15.0
+                              ),
+                            ),
+                            TextButton(
+                                onPressed: (){},
+                                child: const Text(
+                                    'Sign Up',
+                                  style: TextStyle(
+                                    decoration: TextDecoration.underline,
+                                    fontSize: 15.0,
+                                  ),
+                                )
+                            )
+                          ],
+                        ),
+                    ),
+
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            WaveWidget(
+                config: CustomConfig(
+                  colors: [Colors.blue],
+                  heightPercentages: [0.05],
+                  durations: [0],
+
+                ),
+                size: const Size(double.infinity, 106)
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
